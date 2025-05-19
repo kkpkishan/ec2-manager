668640989131446857bee8c57dd5ef3d19b94a0e
Khatrani Kishan <support17@electromech.info>
2025-05-19 11:44:58 +0530

    add the file

--- run-ec2-dashboard.sh
+++ run-ec2-dashboard.sh
@@ -17,8 +17,8 @@ iam-docker-run \
   --name "$CONTAINER_NAME" \
   -d \
   -p 80:5000 \
-  -e USERNAME=admin \
-  -e PASSWORD_HASH=scrypt:32768:8:1$AedvgQJbYOaipHfA$0d1dbfd6296416d1b7ec079b882cf760946ed4fe31652b6bb2a30976b9cc2f3f9187a8c3f328202dc7bb769c5a41ddc5b40b545c1d533b66daa0184b435658d1 \
+  -e USERNAME=vipul.shah@rubamin.com \
+  -e PASSWORD_HASH=scrypt:32768:8:1$ZP8jMy3qFvbyrhaS$4c1ebc350f48a942f0492165f99aad6013a368f309d67058a8a99c55a195319fbf5c925bce395e62ac31daf7789884f1fe093c77d3c5b5cc2716dac4eb79cee9 \
   -e AWS_REGION=ap-south-1 \
   -e FLASK_SECRET_KEY=change_me_to_a_random_string \
   -e FLASK_DEBUG=1 \
